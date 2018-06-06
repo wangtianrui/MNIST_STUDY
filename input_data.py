b8ad6d24599d7d8636edace0b5ec38c961a42fa1
ScorpioMiku <1056992492@qq.com>
2018-06-06 11:48:41 +0800

    Update input_data.py

--- input_data.py
+++ input_data.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
+#
 """Functions for downloading and reading MNIST data."""
 from __future__ import absolute_import
 from __future__ import division
